fb3a09d027c99bd515f9646b2611bc17287f3dc0
Liu Dake <lolitacon2012@gmail.com>
2017-10-10 01:22:00 +0800

    can fetch zhihu message now

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 
-app.use('/', express.static('public/node_web_2'));
+app.use('/', express.static('public/files'));
+
+app.get('/get-api/fetch_zhihu_state',function(req,res){
+	zhihu.User.info("xue-niang").then(function(user){
+  		console.log(user);
+  		res.setHeader('Content-Type', 'application/json');
+    	res.send(user);
+	});
+});
 
 var server = app.listen(8964, function () {
   var host = server.address().address;
--- public/files/js/index.js
+++ public/files/js/index.js
@@ -1,13 +1,18 @@
-
-
 //initialize app vue
 var app = new Vue({
   el: '#app',
   data: {
   	isNotOnTop: false,
     isTooShort: false,
-    useTategaki: true
+    useTategaki: true,
+    zhihu_state: 0,
+    zhihu_message_list: ["zhihu.com"],
+  },
+  computed: {
+  zhihu_message: function () {
+    return this.zhihu_message_list[this.zhihu_state];
   }
+}
 })
 
 //initialize variables
@@ -21,3 +26,30 @@ $(document).scroll(function() {
 $( window ).resize(function() {
   app.isTooShort = false;
 });
+
+
+//fetch data
+var dataFetch = new Promise((resolve, reject) => {
+	console.log("prepare to fetch");
+  // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+  // In this example, we use setTimeout(...) to simulate async code. 
+  // In reality, you will probably be using something like XHR or an HTML5 API.
+  setTimeout(function(){
+    console.log("fetch done");
+    resolve("Success!"); // Yay! Everything went well!
+  }, 250);
+});
+
+dataFetch.then((successMessage) => {
+	console.log("success");
+  // successMessage is whatever we passed in the resolve(...) function above.
+  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+  console.log("Yay! " + successMessage);
+});
+
+
+//card messages
+setInterval(function(){
+	app.zhihu_state = ((app.zhihu_state + 1)%(app.zhihu_message_list.length));
+	console.log(app.zhihu_state);
+}, 5000);
