7aeb0180a7bc451300ea956aa7440a86f5f3edba
Liu Dake <lolitacon2012@gmail.com>
2017-10-13 00:11:24 +0800

    test steam API

--- app.js
+++ app.js
@@ -2,9 +2,66 @@ var express = require('express');
 var zhihu = require('zhihu');
 var app = express();
 var SteamApi = require('steam-api');
+var http = require("http");
+var https = require("https");
+
+var steamApiKey = '918C2026B22CDA2B960E6F8B4C0E5A17';
+var steamIdKey = '76561198044961500';
  
-var steamUser = new SteamApi.User('918C2026B22CDA2B960E6F8B4C0E5A17', '76561198044961500');
-console.log(steamUser);
+
+var steamUser = new SteamApi.User(steamApiKey, steamIdKey);
+var steamUserStats = new SteamApi.UserStats(steamApiKey, steamIdKey);
+var steamNews = new SteamApi.News(steamApiKey, steamIdKey);
+var steamApp = new SteamApi.App(steamApiKey, steamIdKey);
+var steamPlayer = new SteamApi.Player(steamApiKey, steamIdKey);
+var steamInventory = new SteamApi.Inventory(steamApiKey, steamIdKey);
+var steamItems = new SteamApi.Items(steamApiKey, steamIdKey);
+
+/*======get request sender=======*/
+var getJSON = function(options, onResult)
+{
+    console.log("rest::getJSON");
+
+    var port = options.port == 443 ? https : http;
+    var req = port.request(options, function(res)
+    {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+
+    req.on('error', function(err) {
+    	console.log(err);
+        //res.send('error: ' + err.message);
+    });
+
+    req.end();
+};
+
+var s = {
+    host: 'api.steampowered.com',
+    port: 443,
+    path: '/ISteamUser/GetPlayerSummaries/v0002/?key='+steamApiKey+'&steamids='+steamIdKey,
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+getJSON(s, function(statusCode, result) {
+    // I could work with the result html/json here.  I could also just return it
+    console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+});
+
 
 app.get('/hello', function (req, res) {
   res.send('Hello World!');
