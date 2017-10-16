ab6a6bab69ceeb4cca47943c04480666ac7b5c8c
Liu <liu_d@worksap.co.jp>
2017-10-16 18:31:13 +0800

    fix non-tategaki problem

--- public/files/js/text-data.js
+++ public/files/js/text-data.js
@@ -1,6 +1,7 @@
 //Article content in different languages
 var full_content = {
   ja : {
+    nickname:"巫部親雲上",
     greeting:"やっはろー",
     intro:"<h4>システムエンジニアであり、<br><ruby>巫部親雲上<rt>かんなぎぺえぐみい</rt></ruby>と申します。</h4><h4>大学卒業してから、<br>ずっとシンガポールで働いています。</h4><h4>歴史が好き、<br>古典が好き、<br>神仏にも興味があるので、<br>よく巡礼の旅に出ます。</h4><h4>語学が好き、<br>読書が好き、</h4><h4>今が一番大好きです。</h4>",
     search_bar_title:"綺麗なものを探しに行こう。",
@@ -11,8 +12,9 @@ var full_content = {
     nav_bar_gallery:"ギャラリー"
   },
   epo : {
+    nickname:"Kannagi Peekumij",
     greeting:"Bonvenon.",
-    intro:"<h4>Mi estas programaro inĝeniero el Ĉinio kaj mia nomo estas Kannagi Peekŭmij, <br>nun laboras en Singapuro.</h4><h4>Dum la universitato mi studis computikan sciencon kaj Japana religio.</h4><h4>Mi tre ŝatas orientaj aziaj historioj kaj klasikaj literaturoj, <br>tial mi ofte trovas al temploj, sanktejoj kaj muzeoj.</h4><h4>Ankaŭ mi ŝatas studi lingvojn kaj legi librojn.</h4><h4>Mi esperas, ke vi havas bonan ĉiutagojn!</h4>",
+    intro:"<h4>Mi estas programaro inĝeniero el Ĉinio kaj mia nomo estas Kannagi Peekumii, <br>nun laboras en Singapuro.</h4><h4>Dum la universitato mi studis computikan sciencon kaj Japana religio.</h4><h4>Mi tre ŝatas orientaj aziaj historioj kaj klasikaj literaturoj, <br>tial mi ofte trovas al temploj, sanktejoj kaj muzeoj.</h4><h4>Ankaŭ mi ŝatas studi lingvojn kaj legi librojn.</h4><h4>Mi esperas, ke vi havas bonan ĉiutagojn!</h4>",
     search_bar_title:"Iru serĉi ion belan!",
     search_bar_recommend:"(Ne estas rekomendindaj)",
     nav_bar_home:"Hejmo",
@@ -20,16 +22,20 @@ var full_content = {
     nav_bar_about:"Pri mi",
     nav_bar_gallery:"Galerio"
   },
-  en : {greeting:"Hello there.",
-    intro:"<h4>This is Kannagi Pēkŭmī, a software engineer from China. <br>I'm now working in Singapore.</h4><h4>I'm very interested in east Asian history, religions and classical literature. <br>I spend a lot of time visiting temples, shrines and museums during vacation.</h4><h4>As a linguaphile, I'm also enthusiastic in language studies and linguistics.</h4><h4>And, I love reading books.</h4><h4>I wish you to have a good day.</h4>",
-    search_bar_title:"Looking for something gorgeous?",
+  en : {
+    nickname:"Kannagi Pēkumī",
+    greeting:"Hello there.",
+    intro:"<h4>This is Kannagi Pēkumī, a software engineer from China. <br>I'm now working in Singapore.</h4><h4>I'm very interested in east Asian history, religions and classical literature. <br>I spend a lot of time visiting temples, shrines and museums during vacation.</h4><h4>As a linguaphile, I'm also enthusiastic in language studies and linguistics.</h4><h4>And, I love reading books.</h4><h4>I wish you to have a good day.</h4>",
+    search_bar_title:"Looking for something?",
     search_bar_recommend:"(There is no recommends yet)",
     nav_bar_home:"Home",
     nav_bar_blog:"Blog",
     nav_bar_about:"About",
     nav_bar_gallery:"Gallery"
   },
-  zh : {greeting:"巫部親雲上",
+  zh : {
+    nickname:"Kannagi Pēkumī",
+    greeting:"巫部親雲上",
     intro:"<h4>来自華北的普通程序員，現居星洲。</h4><h4>喜歓東亞宗教、歴史和古典文学，<br>常出入扶桑各地神社寺庙以及考古博物館，<br>樂此不疲。</h4><h4>粗通洋文，對語言学有所涉獵，<br>但除英日二語之外尚不足以流暢交談。</h4><h4>好讀書不求甚解，<br>關注技術而欠缺實践。</h4><h4>對世界充滿好奇，<br>姑且算是個熱愛生活的人吧。</h4>",
     search_bar_title:"在找点什么呢？",
     search_bar_recommend:"（暫無推荐）",
